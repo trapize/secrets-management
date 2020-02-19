c834cdcc82bd246f33f7fbd47f5787b7f743104c
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2020-02-18 20:17:29 -0500

    exports secret symbols as Secrets

--- src/secrets/index.ts
+++ src/secrets/index.ts
@@ -2,4 +2,5 @@ export * from './IKey.Management.Service.Client';
 export * from './ISecret.App.Settings';
 export * from './ISecret.Service';
 export * from './IStorage.Client';
-export * from './Secrets.Symbols';
+import * as Secrets from './Secrets.Symbols';
+export { Secrets }
